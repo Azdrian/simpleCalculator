faac4aac22ba12f86b4914bc1897fd68b49c29ad
Azdrian <azdrian22@gmail.com>
2017-05-12 02:07:54 -0400

    minor changes made to calculator

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,16 @@
 /* coding here */
 
-(function(){
+
+// sets up array for inputs
+$input =[];
+ 
+//get element by ID
+function getId(x) {
+	return document.getElementById(x);
+}
+
+//getId("one").addEventListener('click', );
+console.log(getId("one"));
 
 
 
@@ -8,4 +18,28 @@
 
 
 
-})();
+
+
+
+/*
+$update;
+
+$output;
+*/
+/*
+switch (expression) {
+  case value1:
+    //Statements executed when the result of expression matches value1
+    [break;]
+  case value2:
+    //Statements executed when the result of expression matches value2
+    [break;]
+  ...
+  case valueN:
+    //Statements executed when the result of expression matches valueN
+    [break;]
+  default:
+    //Statements executed when none of the values match the value of the expression
+    [break;]
+}
+*/
