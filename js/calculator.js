1ae58e7a333f00328cf37ee6d062e060d07c8794
Azdrian <azdrian22@gmail.com>
2017-05-17 22:09:17 -0400

    minior changes made to calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -25,26 +25,48 @@
           $inputs.push(getId("display").textContent= $defaultValue);
           $powerStatus = true;
           console.log("I'm on!");
-          console.log($powerStatus);
-          
+          //console.log($powerStatus);
+                  
+        while ($powerStatus === true){
+           getId("one").addEventListener('click',function(){
+              console.log("heyyy im 1");
+            /*
+             $inputs.push(1);
+             //console.log($inputs);
+             for(i=0, len=$inputs.length; i < len; i++){
+                    // getId("display").textContent= $inputs[i];
+                    console.log($inputs[i]);
+             } 
+             */
+          });
+       }//end of while loop
+
+
+
+
+
+
+
+
+
        }else{
           $inputs.push($inputs=[] );
           getId("display").textContent=$inputs;
           $powerStatus = false;
           console.log("inputArray: " + $inputs + " " + "zeroVar: " +  $defaultValue);
           console.log("I'm off!");
-          console.log($powerStatus);
+          //console.log($powerStatus);
 
 
        }
   });
-console.log($powerStatus);
+  /*
 
   //Clear the screen
   getId("clear").addEventListener("click", function clearScreen(){
     console.log($inputs);
    /* var $emptyInput= $inputs.push($inputs=[] );
    console.log($emptyInput);
-   */
+   
   });
-
+*/
